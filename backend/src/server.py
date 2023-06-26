95d379befc4110355237bd94cb8a2571f106971f
Wilson <71456315+wilsonw13@users.noreply.github.com>
2023-06-25 22:06:09 -0400

    figured out a solution to communicating with the distalgo script

--- backend/src/server.py
+++ backend/src/server.py
@@ -1,11 +1,23 @@
 import asyncio
 from websockets.server import serve
+import threading
+import subprocess
+import os
 import json
 
 from main import query_prerequisite_graph
 
-url = "localhost"
-port = 3003
+with open("config.json", "r") as f:
+    config = json.load(f)
+    host, port = config["ws_host"], config["ws_port"]
+    assert host and port, "Failed to load host and port from config.json!"
+
+
+def test():
+    print("Running subprocess...")
+    subprocess.run(f"py -m da --rules src/test.da",
+                   env={**os.environ, "PYTHONPATH": "src:da:ps"}
+                   )
 
 
 async def handler(websocket):
@@ -14,16 +26,26 @@ async def handler(websocket):
         msgData = json.loads(message)
 
         # checks the type of message
-        if msgData["type"] == "query":
+        if msgData.get("type") == "query":
 
             # queries the graph and sends the response back to the client
             response = query_prerequisite_graph(**msgData["query"])
             await websocket.send(json.dumps({"type": "graph", "data": response}))
 
+        else:
+            print("Received unknown message type: ")
+            print(msgData)
+
 
 async def main():
-    async with serve(handler, url, port):
-        print(f"Server running at http://{url}:{port}")
+    async with serve(handler, host, port):
+        print(
+            f"Server running at ws://{host}:{port}")
+
+        # start the test thread
+        test_thread = threading.Thread(target=test)
+        test_thread.start()
+
         await asyncio.Future()  # run forever
 
 asyncio.run(main())
--- backend/src/test2.py
+++ backend/src/test2.py
@@ -1,3 +1,7 @@
 import subprocess
+import os
 
-subprocess.run(['python', '-m', 'da', '--rules', 'src/test.da'])
+print("HI FROM TEST2")
+subprocess.run(f"py -m da --rules src/test.da",
+               env={**os.environ, "PYTHONPATH": "src:da:ps"}
+               )
